68d933c160cc23ada7c501296bd3b2282b245472
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2023-09-06 12:26:04 +0600

    Navbar changed and features edited

--- components/Features.js
+++ components/Features.js
@@ -8,13 +8,105 @@ import { FaEthereum } from 'react-icons/fa';
 const Features = () => {
     const [love, setLove] = useState(false);
     // console.log(love);
+    const features = [
+        {
+            id: 1,
+            imgClass: 'card1-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 2,
+            imgClass: 'card2-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 3,
+            imgClass: 'card3-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 4,
+            imgClass: 'card4-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 5,
+            imgClass: 'card5-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 6,
+            imgClass: 'card6-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 7,
+            imgClass: 'card3-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        },
+        {
+            id: 8,
+            imgClass: 'card7-bg-image',
+            title: 'Monkey #555',
+            postBy: 'By Admin',
+            bidNumber: '12.2 ETH'
+        }
+    ]
     return (
         <div className='FeaturesBgImages'>
             <div className='max-w-7xl lg:mx-auto md:mx-8 mx-5 md:py-20 py-14'>
                 <h1 className='text-center common-header-gradient2 lg:text-4xl md:text-3xl text-3xl font-bold mb-12 uppercase'>Trending Auctions</h1>
 
                 <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5'>
-                    <div className='common-card-css hover:border-[#5B1FDC] transition-all duration-500 p-3'>
+                    {
+                        features.map(feature => {
+                            return (
+                                <div key={feature?.id} className='common-card-css hover:border-[#5B1FDC] transition-all duration-500 p-3'>
+                                    <div className={`${feature?.imgClass} bg-cover bg-no-repeat bg-center h-44 rounded-2xl`}></div>
+                                    <div className='flex items-center justify-between py-8'>
+                                        <div className='flex items-center gap-3'>
+                                            <Image src={FeatureBrand} width={60} height={70} alt='Feature Brand logo' className='rounded-xl' />
+                                            <div className='flex flex-col gap-1'>
+                                                <h3 className='text-white font-medium text-base'>{feature.title}</h3>
+                                                <p className='text-gray-400 text-sm'>{feature?.postBy}</p>
+                                            </div>
+                                        </div>
+                                        <div className='border border-[#ffcbcf3d] rounded-full p-2 cursor-pointer' onClick={() => setLove(feature?.id)}>
+                                            {love === feature?.id ?
+                                                <HiHeart className='text-[#FF434E] text-lg' />
+                                                :
+                                                <HiOutlineHeart className='text-[#FF434E] text-lg ' />
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className='flex items-center justify-between'>
+                                        <div className='flex items-center gap-3'>
+                                            <div className='p-2 bg-[#5B40FF] rounded-full'>
+                                                <FaEthereum className="text-xl text-white" />
+                                            </div>
+                                            <p className='text-base text-white font-semibold'>{feature?.bidNumber}</p>
+                                        </div>
+                                        <button className='border border-[#1BF1B1] hover:border-[#5721CB] hover:bg-[#5721CB] transition-all duration-500 rounded-full py-2 px-5 text-white text-sm'>Place a Bid</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                    {/* <div className='common-card-css hover:border-[#5B1FDC] transition-all duration-500 p-3'>
                         <div className='card1-bg-image bg-cover bg-no-repeat bg-center h-44 rounded-2xl'></div>
                         <div className='flex items-center justify-between py-8'>
                             <div className='flex items-center gap-3'>
@@ -237,7 +329,7 @@ const Features = () => {
                             </div>
                             <button className='border border-[#1BF1B1] hover:border-[#5721CB] hover:bg-[#5721CB] transition-all duration-500 rounded-full py-2 px-5 text-white text-sm'>Place a Bid</button>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='flex justify-center mt-12'>
                     <button className='uppercase text-base font-medium text-white bg-[#5721CB] hover:bg-transparent duration-500 border border-[#5721CB] rounded-full py-2.5 lg:px-10 px-8'>Explore</button>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,84 +1,176 @@
-import Link from 'next/link';
-import React, { useState } from 'react';
-import { HiMenu } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai';
+// import Link from 'next/link';
+// import React, { useState } from 'react';
+// import { HiMenu } from 'react-icons/hi';
+// import { AiOutlineClose } from 'react-icons/ai';
+// import Logo from '../public/Logos/logo.png';
+// import Image from 'next/image';
+// import { FaDiscord, FaTwitter } from 'react-icons/fa';
+// import WIcon from '../public/icons/w-icon.png';
+
+// export default function Navbar() {
+//     const [open, setOpen] = useState(false);
+//     return (
+//         <nav className={`absolute z-10 top-0 w-full py-1.5 ${open ? 'bg-[#0E102B]' : 'bg-transparent'}`}>
+//             <div className="justify-between px-4 lg:mx-auto md:mx-5 mx-0 lg:max-w-7xl lg:items-center lg:flex lg:px-8">
+//                 <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
+//                     <Link href='/'><Image src={Logo} width={100} height={100} alt='Logo' /></Link>
+//                     <div className="lg:hidden flex items-center gap-3">
+//                         {/* Hamburger toggle button */}
+//                         <button className="p-2 text-gray-700 rounded-md outline-none" onClick={() => setOpen(!open)} >
+//                             {open ? (
+//                                 <svg
+//                                     xmlns="http://www.w3.org/2000/svg"
+//                                     className="w-6 h-6 dark:text-white"
+//                                     viewBox="0 0 20 20"
+//                                     fill="currentColor"
+//                                 >
+//                                     <path
+//                                         fillRule="evenodd"
+//                                         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+//                                         clipRule="evenodd"
+//                                     />
+//                                 </svg>
+//                             ) : (
+//                                 <svg
+//                                     xmlns="http://www.w3.org/2000/svg"
+//                                     className="w-6 h-6 dark:text-white"
+//                                     fill="none"
+//                                     viewBox="0 0 24 24"
+//                                     stroke="currentColor"
+//                                     strokeWidth={2}
+//                                 >
+//                                     <path
+//                                         strokeLinecap="round"
+//                                         strokeLinejoin="round"
+//                                         d="M4 6h16M4 12h16M4 18h16"
+//                                     />
+//                                 </svg>
+//                             )}
+//                         </button>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className={`lg:flex lg:items-center lg:justify-between lg:pb-0 pb-12 absolute lg:static lg:bg-transparent bg-[#0E102B] lg:z-auto -z-10 left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+//                         <nav className="lg:flex grid grid-cols-1 items-center justify-center lg:gap-5 gap-5 lg:pt-0 pt-8">
+//                             <Link href='/' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Home</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Marketplace</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Collection</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Community</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Create</Link>
+//                         </nav>
+
+//                         <div className="mt-8 lg:hidden inline-block">
+//                             <ul className='flex items-center gap-4'>
+//                                 {/* <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link> */}
+//                                 {/* <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link> */}
+//                                 <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='flex items-center lg:gap-5 gap-5'>
+//                     <div className="space-x-2 lg:inline-block hidden">
+//                         <ul className='flex items-center gap-4'>
+//                             {/* <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link> */}
+//                             {/* <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link> */}
+//                             <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-3 px-10 font-medium  flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
+//                         </ul>
+//                     </div>
+//                 </div>
+
+//             </div>
+//         </nav>
+//     )
+// }
+
+import React from "react";
 import Logo from '../public/Logos/logo.png';
-import Image from 'next/image';
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
+import { Header, Container, Group, Burger, Transition, Divider, Box, Button } from "@mantine/core";
+import { useDisclosure, useHeadroom, useWindowScroll } from "@mantine/hooks";
+import Link from "next/link";
+import Image from "next/image";
+// import { links } from "../data/NavbarData";
 import WIcon from '../public/icons/w-icon.png';
 
-export default function Navbar() {
-    const [open, setOpen] = useState(false);
+const HeaderView = () => {
+    const [opened, { toggle }] = useDisclosure(false);
+    const pinned = useHeadroom({ fixedAt: 120 });
+    const [scroll] = useWindowScroll();
+    const HEADER_HEIGHT = scroll.y >= 765 ? 85 : 110;
+
+    // const items = links.map((link) => (
+    //     <a
+    //         key={link.label}
+    //         href={link.link}
+    //         target="_blank"
+    //         rel="noreferrer"
+    //         className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 uppercase text-white nav-item`}
+    //     >
+    //         <span>{link.label}</span>
+    //     </a>
+    // ));
+
     return (
-        <nav className={`absolute z-10 top-0 w-full py-1.5 ${open ? 'bg-[#0E102B]' : 'bg-transparent'}`}>
-            <div className="justify-between px-4 lg:mx-auto md:mx-5 mx-0 lg:max-w-7xl lg:items-center lg:flex lg:px-8">
-                <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
-                    <Link href='/'><Image src={Logo} width={100} height={100} alt='Logo' /></Link>
-                    <div className="lg:hidden flex items-center gap-3">
-                        {/* Hamburger toggle button */}
-                        <button className="p-2 text-gray-700 rounded-md outline-none" onClick={() => setOpen(!open)} >
-                            {open ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-6 h-6 dark:text-white"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-6 h-6 dark:text-white"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                </svg>
-                            )}
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <div className={`lg:flex lg:items-center lg:justify-between lg:pb-0 pb-12 absolute lg:static lg:bg-transparent bg-[#0E102B] lg:z-auto -z-10 left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
-                        <nav className="lg:flex grid grid-cols-1 items-center justify-center lg:gap-5 gap-5 lg:pt-0 pt-8">
-                            <Link href='/' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Home</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Marketplace</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Collection</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Community</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Create</Link>
-                        </nav>
+        <Header height={HEADER_HEIGHT} px="md"
+            className={`z-50 py-3 px-0 absolute -translate-y-0 border-none ${opened ? 'bg-[#0E102B]' : 'bg-transparent'}`}
+        >
+            <Container className="flex justify-between items-center h-full lg:px-12" size="xl">
+                <Box>
+                    <Link href="/" className="flex justify-start items-center">
+                        <Image src={Logo} width={100} height={100} alt='Logo' />
+                    </Link>
+                </Box>
 
-                        <div className="mt-8 lg:hidden inline-block">
-                            <ul className='flex items-center gap-4'>
-                                {/* <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link> */}
-                                {/* <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link> */}
-                                <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex items-center lg:gap-5 gap-5'>
-                    <div className="space-x-2 lg:inline-block hidden">
-                        <ul className='flex items-center gap-4'>
-                            {/* <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link> */}
-                            {/* <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link> */}
-                            <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-3 px-10 font-medium  flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
-                        </ul>
-                    </div>
-                </div>
+                <Group spacing={0} className="hidden md:flex items-center lg:gap-8 md:gap-4">
+                    <Link href="/" target="_blank" rel="noreferrer" className='font-semibold transition-all duration-300 text-white hover:text-[#1BF1B1]'>Home</Link>
+                    <Link href="#" target="_blank" rel="noreferrer" className='font-semibold transition-all duration-300 text-white hover:text-[#1BF1B1]'>Marketplace</Link>
+                    <Link href="#" target="_blank" rel="noreferrer" className='font-semibold transition-all duration-300 text-white hover:text-[#1BF1B1]'>Collection</Link>
+                    <Link href="#" target="_blank" rel="noreferrer" className='font-semibold transition-all duration-300 text-white hover:text-[#1BF1B1]'>Community</Link>
+                    <Link href="#" target="_blank" rel="noreferrer" className='font-semibold transition-all duration-300 text-white hover:text-[#1BF1B1]'>Create</Link>
+                </Group>
 
-            </div>
-        </nav>
-    )
-}
+                <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full lg:py-3 md:py-2 lg:px-10 px-5 font-medium hidden md:flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
+
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    className="md:hidden"
+                    color="#fff"
+                    size="md"
+                />
+                {/* Phone */}
+                <Transition transition="pop-top-right" duration={200} mounted={opened}>
+                    {(styles) => (
+                        <Box
+                            className="absolute bg-[#0E102B] left-0 w-full z-10 overflow-hidden md:hidden rounded-none pb-5 overflow-y-auto"
+                            top={HEADER_HEIGHT}
+                            style={styles}
+                        >
+                            <Link href="/" target="_blank" rel="noreferrer" className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 text-white nav-item`} >
+                                <span>Home</span>
+                            </Link>
+                            <Link href="#" target="_blank" rel="noreferrer" className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 text-white nav-item`} >
+                                <span>Marketplace</span>
+                            </Link>
+                            <Link href="#" target="_blank" rel="noreferrer" className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 text-white nav-item`} >
+                                <span>Collection</span>
+                            </Link>
+                            <Link href="#" target="_blank" rel="noreferrer" className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 text-white nav-item`} >
+                                <span>Community</span>
+                            </Link>
+                            <Link href="#" target="_blank" rel="noreferrer" className={`block relative text-base md:text-sm 2xl:text-base font-semibold px-6 py-3 transition-all duration-300 text-white nav-item`} >
+                                <span>Create</span>
+                            </Link>
+
+                            <Link href="" rel="noreferrer" target="_blank" className="mx-5">
+                                <Button variant="white" fullWidth className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center justify-center'><Image src={WIcon} alt='W-Icon' className="mr-2" />Connect Wallet</Button>
+                            </Link>
+                        </Box>
+                    )}
+                </Transition>
+            </Container>
+        </Header>
+    );
+};
+
+export default HeaderView;
